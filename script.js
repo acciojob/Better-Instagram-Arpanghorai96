f6fe538033f4214d31de4638e0483e3c96eaa95c
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-05-06 09:04:40 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+const divs=document.querySelectorAll(".image");
+
+let dragableItem=null;
+function ondragStart(event){
+    dragableItem=event.target;
+}
+function ondragOver(event)
+{
+    event.preventDefault();
+}
+function onDrop(event){
+    event.preventDefault();
+    // Get the image of the target item
+    let targetImage = event.target.style.backgroundImage;
+    
+    // Swap the images of the dragged and target items
+    event.target.style.backgroundImage = dragableItem.style.backgroundImage;
+    dragableItem.style.backgroundImage = targetImage;
+}
+
+divs.forEach((div) => {
+    div.addEventListener("dragstart", ondragStart);
+    div.addEventListener("dragover", ondragOver);
+    div.addEventListener("drop", onDrop);
+});
